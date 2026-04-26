f8664ca767006d3189e884a3eeb937c53363f923
SaajidhNawwar <mnmsaajidh@gmail.com>
2026-04-26 13:13:58 +0530

    1.13pm

--- playwright.service.config.js
+++ playwright.service.config.js
@@ -3,20 +3,23 @@ const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
 const { ClientSecretCredential } = require('@azure/identity');
 const config = require('./playwright.config');
 
-
 module.exports = defineConfig(
   config,
   createAzurePlaywrightConfig(config, {
     exposeNetwork: '<loopback>',
     connectTimeout: 3 * 60 * 1000,
     os: ServiceOS.LINUX,
-    credential,
+    credential: new ClientSecretCredential(  // 👈 this was missing
+      process.env.AZURE_TENANT_ID,
+      process.env.AZURE_CLIENT_ID,
+      process.env.AZURE_CLIENT_SECRET,
+    ),
   }),
   {
     reporter: [
       ['line'],
-      ["html", { open: "never" }],
-      ["@azure/playwright/reporter"],  // ✅ works in 1.1.5
+      ['html', { open: 'never' }],
+      ['@azure/playwright/reporter'],
     ],
   }
 );
